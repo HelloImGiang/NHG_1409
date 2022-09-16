076606f1a102dacc8022cc57f63a141e97f07e39
NguyenHuongGiang128 <huonggiang12082001@gmail.com>
2022-09-16 13:04:31 +0700

    NHG_1409

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace NguyenHuongGiang_BTH.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
