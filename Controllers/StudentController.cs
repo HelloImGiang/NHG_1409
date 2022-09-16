10614b776fcccec101837c91759fd9494915f5ef
NguyenHuongGiang128 <huonggiang12082001@gmail.com>
2022-09-16 13:51:38 +0700

    NHG_1409

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,41 @@
+using Microsoft.AspNetCore.Mvc;
+using NguyenHuongGiang_BTH.Models;
+
+namespace NguyenHuongGiang_BTH.Controllers
+{
+public class StudentController : Controller 
+{
+    public IActionResult Index()
+    {
+            // khơi tao list student
+            List<Student> StdList = new List<Student>()
+            {
+                new Student {StudentID = 1, StudentName = "Nguyen Huong Giang", StudentAge = 21},
+                new Student {StudentID = 2, StudentName = "Nguyen Huong G", StudentAge = 25},
+                new Student {StudentID = 3, StudentName = "Nguyen Huong Gi", StudentAge = 18},
+                new Student {StudentID = 4, StudentName = "Nguyen Huong Gia", StudentAge = 2},
+                new Student {StudentID = 5, StudentName = "Nguyen Huong Gian", StudentAge = 1}
+
+            };
+            ViewData["Student"] = StdList;
+            return View(StdList);
+    }
+[HttpGet]     
+public IActionResult Create()
+{
+    return View();
+}
+[HttpPost]
+    public IActionResult Create(Student std)   
+        {
+            string message = std.StudentID + "-";
+            message += std.StudentName +"-";
+            message += std.StudentAge;
+            ViewBag.SinhVien = message;
+            return View();
+            // ViewBag.SinhVien = "Hello: " + std.StudentID + "-" + std.StudentName + "-" + std.StudentAge;
+            // return View();
+            
+        }
+    }
+}
---
+++ Models/Student.cs
@@ -0,0 +1,10 @@
+namespace NguyenHuongGiang_BTH.Models
+{
+    public class Student 
+    {
+        //khai bao 3 thuoc tinh cua doi tuong
+        public int StudentID {get; set;}
+        public string StudentName {get; set;}
+        public int StudentAge {get; set;}
+    }
+}
